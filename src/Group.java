bb05c4160acc2f8d9f1165074c9cc40e85f1bebf
manoharan <pr971100@dal.ca>
2020-02-19 20:51:02 -0400

    test commit 3

---
+++ src/Group.java
@@ -0,0 +1,19 @@
+//Stores the information of the grouped cells
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Group{
+
+    private int result = 0;
+    private String operator = "";
+    private List groupCells = new ArrayList();
+
+
+    public static void main(String args[]){
+        System.out.println("321");
+    }
+
+
+
+}
