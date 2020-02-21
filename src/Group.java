2a95859ddc41475855d9a7b8b4af9469f75bb17e
manoharan <pr971100@dal.ca>
2020-02-21 16:31:37 -0400

    load method

--- src/Group.java
+++ src/Group.java
@@ -5,15 +5,84 @@ import java.util.List;
 
 public class Group{
 
-    private int result = 0;
-    private String operator = "";
-    private List groupCells = new ArrayList();
+    //name of the group
+    private Character name;
 
+    //result of the group
+    private int result;
 
-    public static void main(String args[]){
-        System.out.println("321");
+    //operator of the group
+    private Character operator;
+
+    //valid operators
+    private List<Character> validOperators;
+
+    /*
+    getValidOperators method
+    set the list of valid operators
+    returns the list
+     */
+    public List getValidOperators(){
+        if(this.validOperators == null){
+            this.validOperators = new ArrayList<>();
+            this.validOperators.add('+');
+            this.validOperators.add('-');
+            this.validOperators.add('*');
+            this.validOperators.add('/');
+            this.validOperators.add('=');
+        }
+        return this.validOperators;
     }
 
+    /*
+    setResult method
+    gets the result of the group as input
+    sets the input in the result of this object
+     */
+    public void setResult(int result){
+        this.result = result;
+    }
 
+    /*
+    getResult method
+    returns the result of the group
+     */
+    public int getResult(){
+        return this.result;
+    }
+
+    /*
+    setOperator method
+    gets the operator of the group as input
+    sets the input in the operator of this object
+    */
+    public void setOperator(Character operator){
+        this.operator = operator;
+    }
+
+    /*
+    getOperator method
+    returns the operator of the group
+    */
+    public Character getOperator(){
+        return this.operator;
+    }
+
+    /*
+    setName method
+    gets the name of the group as input
+    sets the input in the name of this object
+    */
+    public void setName(Character name){
+        this.name = name;
+    }
+
+    /*
+    getName method
+    returns the name of the group
+    */
+    public Character getName(){
+        return this.operator;
+    }
 
 }
--- src/Mathdoku.java
+++ src/Mathdoku.java
@@ -1,8 +1,34 @@
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 //class is used to load, solve and print the puzzle
 
 public class Mathdoku {
+
+    //size of the puzzle
+    private int size = 0;
+
+    //number of groups in the puzzle
+    private int noOfGrps = 0;
+
+    //actual final puzzle
+    private List<List<Integer>> puzzle = new ArrayList<>();
+
+    //group name of the each cell
+    private List<List<Character>> grpNameOfCell = new ArrayList<>();
+
+    //list of groups
+    private List<Group> groups = new ArrayList<>();
+
+    /*
+    printString method
+    gets the input string and prints that to user
+     */
+    private void printString (String input){
+        System.out.println(input);
+    }
 
     /*
     loadPuzzle method
@@ -10,9 +36,55 @@ public class Mathdoku {
     creates a puzzle with the input
      */
     public boolean loadPuzzle(BufferedReader stream) {
-        boolean isLoaded = false;
+        boolean isLoaded = true;
+        String space = " ";
+        try {
+            if (stream != null) {
+                String line = null;
+                int overAllLineNo = 0;
+                int lineNoForSize = 0;
+                int lineNoForGrps = 0;
+                while ((line = stream.readLine()) != null) {
+                    if(line.contains(space)){
+                        char[] grpDesc = line.toCharArray();
+                        if(grpDesc.length == 5) {
+                            Group grp = new Group();
+                            grp.setName(grpDesc[0]);
+                            grp.setResult(Integer.valueOf(String.valueOf(grpDesc[2])));
+                            grp.setOperator(grpDesc[4]);
+                            groups.add(grp);
+                        } else{
+                            printString("Group description is wrong/without enough information in line number "+overAllLineNo);
+                            isLoaded = false;
+                            break;
+                        }
+                        lineNoForGrps = lineNoForGrps + 1;
+                    } else{
+                        char[] grpNames = line.toCharArray();
+                        List<Character> cells = new ArrayList<>();
+                        for(char grpName : grpNames){
+                            cells.add(grpName);
+                        }
+                        grpNameOfCell.add(lineNoForSize, cells);
+                        lineNoForSize = lineNoForSize + 1;
+                    }
+                    overAllLineNo = overAllLineNo + 1;
+                }
 
+                size = lineNoForSize;
+                noOfGrps = lineNoForGrps;
 
+            } else {
+                printString("Input stream is empty.");
+                isLoaded = false;
+            }
+        } catch (IOException e){
+            isLoaded = false;
+            printString("Exception in reading the input stream in loadPuzzle method.");
+        } catch (Exception e){
+            isLoaded = false;
+            printString("System faced unexpected exception in loadPuzzle method.");
+        }
         return isLoaded;
     }
 
@@ -24,6 +96,8 @@ public class Mathdoku {
     public boolean readyToSolve() {
         boolean isReady = false;
 
+
+
         return isReady;
     }
 
@@ -33,6 +107,8 @@ public class Mathdoku {
      */
     public boolean solve() {
         boolean isSolved = false;
+
+
 
         return isSolved;
     }
